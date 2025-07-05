9a235f7c1af9e89b715dd347b98f987830e11623
Gabriela Stancu <87585067+gabstancu@users.noreply.github.com>
2025-07-05 16:44:41 +0300

    Completed BoundaryCondition.

--- include/basis/BoundaryConditions.hpp
+++ include/basis/BoundaryConditions.hpp
@@ -4,8 +4,10 @@
 #include <ginac/ginac.h>
 #include <optional>
 #include <variant>
+#include <iostream>
 
 enum class Side { Top, Bottom, Left, Right };
+enum class Type { Dirichlet, Neumann, Robin };
 
 using BoundaryType = std::variant<GiNaC::ex, double>;
 
@@ -13,6 +15,7 @@ struct BoundaryCondition
 {
     std::optional<BoundaryType> expr = std::nullopt;
     Side side;
+    Type type;
 
     bool is_set()
     {
@@ -28,6 +31,33 @@ struct BoundaryCondition
     {
         return expr.has_value() && std::holds_alternative<double>(expr.value());   
     }
+
+    double evaluate (std::pair<int, int> coordinates, 
+                     std::vector<GiNaC::symbol> vars, 
+                     double h)
+    {
+        double val = 0.0;
+        int i = coordinates.first, j = coordinates.second;
+
+        if (this->is_set())
+        {
+            if (this->is_constant())
+            {   
+                val = std::get<double>(*expr);
+                return val;
+            }
+            else if (this->is_expression())
+            {
+                GiNaC::exmap m; 
+                m[vars[0]] = j * h; m[vars[1]] = i * h;
+                GiNaC::ex b_expr = std::get<GiNaC::ex>(*expr);
+                GiNaC::ex evaluated_boundary = b_expr.subs(m).evalf();
+                val = GiNaC::ex_to<GiNaC::numeric>(evaluated_boundary).to_double();
+                return val;
+            }
+        }
+        return val;
+    }
 };
 
 
@@ -37,6 +67,7 @@ struct BoundaryConditions
                       bottom{std::nullopt, Side::Bottom}, 
                       left{std::nullopt, Side::Left}, 
                       right{std::nullopt, Side::Right} ;
+
 };
 
 
--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,5 +1,3 @@
-#include "solvers.hpp"
-
 void setup_benchmarking ()
 {
 
